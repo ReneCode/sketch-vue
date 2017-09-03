b865808224bf1ab2e11114067c213c0ca02e438d
rene_imac <HarryMillerMail@googlemail.com>
2017-09-03 08:45:31 +0200

    ia-rect use ia-two-point

--- src/interaction/ia-rect.js
+++ src/interaction/ia-rect.js
@@ -1,5 +1,6 @@
 import IaBase from './ia-base'
 import selectionList from '@/store/selectionList';
+import interaction from '@/interaction';
 import ItemRectangle from '@/model/item-rectangle';
 // import store from '@/store';
 
@@ -15,50 +16,64 @@ export default class IaRect extends IaBase {
 
   onMouseDown(event) {
     selectionList.clear();
-    this.startPoint = this.getSVGPoint(event);
-    this.item = new ItemRectangle(this.startPoint, this.startPoint);
-    this.tmpItems.push(this.item);
+    let ia = interaction.start('iaTwoPoints', (err, payload) => {
+      return this.twoPointsCallback(err, payload);
+    });
+    ia.onMouseDown(event);
+
+    // this.startPoint = this.getSVGPoint(event);
+    // this.item = new ItemRectangle(this.startPoint, this.startPoint);
+    // this.tmpItems.push(this.item);
     return false;
   }
 
-  onMouseUp(event) {
-    this.setPoint2(event);
-    this.startPoint = null;
-    this.clearTempItems();
+  twoPointsCallback(err, payload) {
+    if (err) {
+      this.clearUp();
+      return "stop";
+    }
+    switch (payload.event) {
+      case "onMouseDown":
+        this.setRect(payload.pt1, payload.pt1);
+        const item = {
+          svg: this.rect
+        }
+        this.tmpItems.push(item);
+        break;
+      case "onMouseMove":
+        this.setRect(payload.pt1, payload.pt2);
+        break;
+      case "onMouseUp":
+        this.finishRectangle(payload);
+        break;
+    }
+  }
 
-    if (this.item.svg.width === 0 || this.item.svg.height === 0) {
+  finishRectangle(payload) {
+    this.clearUp();
+    let delta = payload.pt1.sub(payload.pt2);
+    if (delta.x === 0 || delta.y === 0) {
       this.emit("zero rect", null)
     } else {
-      this.emit(null, this.item);
+      let item = new ItemRectangle(payload.pt1, payload.pt2);
+      this.emit(null, item);
     }
   }
 
-  onMouseMove(event) {
-    if (this.startPoint) {
-      this.setPoint2(event);
-    }
-  }
-
-  onKeyDown(event) {
-    if (event.keyCode === 27) {
-      this.clearTempItems();
-      this.startPoint = null;
-      this.emit("escape", null);
-    }
-  }
-
-  save() {
-  }
-
-  clearTempItems() {
+  clearUp() {
     this.tmpItems.splice(0);
+    this.startPoint = null;
+    this.rect = null;
   }
 
-  setPoint2(event) {
-    if (this.item) {
-      const p2 = this.getSVGPoint(event);
-      this.item.setFromTwoPoints(this.startPoint, p2);
+  setRect(pt1, pt2) {
+    if (!this.rect) {
+      this.rect = {};
     }
+    this.rect.x = Math.min(pt1.x, pt2.x);
+    this.rect.y = Math.min(pt1.y, pt2.y);
+    let delta = pt2.sub(pt1).abs();
+    this.rect.width = delta.x;
+    this.rect.height = delta.y;
   }
-
 }
