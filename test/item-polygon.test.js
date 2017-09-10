f95087c7597cb82c188d6bbb23a70a10282e0594
rene_imac <HarryMillerMail@googlemail.com>
2017-09-10 17:58:26 +0200

    testing in travis ci

--- test/item-polygon.test.js
+++ test/item-polygon.test.js
@@ -21,53 +21,15 @@ describe('ItemPolygon', () => {
     item.svg.points[0].y.should.be.equal(100);
   });
 
-  /*
-  it('should construct ItemRectangle(p1,p2)', () => {
-    const p1 = new Point(50, 100);
-    const p2 = new Point(150, 300);
-    let item = new ItemRectangle(p1, p2);
-    item.svg.type.should.be.equal('rect');
-    item.svg.x.should.be.equal(50);
-    item.svg.y.should.be.equal(100);
-    item.svg.width.should.be.equal(100);
-    item.svg.height.should.be.equal(200);
+  it('clone() should create Point instances', () => {
+    let item = new ItemPolygon();
+    const pt = new Point(50, 100);
+    item.addPoint(pt);
+    const clone = item.clone();
+    clone.svg.points.length.should.be.equal(1);
+    (clone.svg.points[0] instanceof Point).should.be.true;
+    clone.svg.points[0].x.should.be.equal(50);
+    clone.svg.points[0].y.should.be.equal(100);
   });
 
-  it('should clone ItemRectangle', () => {
-    const p1 = new Point(50, 100);
-    const p2 = new Point(150, 300);
-    const org = new ItemRectangle(p1, p2);
-    const item = org.clone();
-    item.svg.type.should.be.equal('rect');
-    item.svg.x.should.be.equal(50);
-    item.svg.y.should.be.equal(100);
-    item.svg.width.should.be.equal(100);
-    item.svg.height.should.be.equal(200);
-    (item instanceof ItemRectangle).should.be.true
-  });
-
-  it('should move() ItemRectangle', () => {
-    const p1 = new Point(50, 100);
-    const p2 = new Point(150, 300);
-    const item = new ItemRectangle(p1, p2);
-    const delta = new Point(10, 20);
-    item.move(delta);
-    item.svg.x.should.be.equal(60);
-    item.svg.y.should.be.equal(120);
-  });
-
-  it('move() should not modify orginal ItemRectangle', () => {
-    const p1 = new Point(50, 100);
-    const p2 = new Point(150, 300);
-    const org = new ItemRectangle(p1, p2);
-    const item = org.clone();
-    const delta = new Point(10, 20);
-    item.move(delta);
-    item.svg.type.should.be.equal('rect');
-    item.svg.x.should.be.equal(60);
-    item.svg.y.should.be.equal(120);
-    org.svg.x.should.be.equal(50);
-    org.svg.y.should.be.equal(100);
-  });
-  */
 });
