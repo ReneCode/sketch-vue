b22dff4ab85685e73d8fe7ad1573130d0a4737ae
rene_imac <HarryMillerMail@googlemail.com>
2017-09-16 16:01:25 +0200

    fix getSVGPoint

--- src/svg/svg-transform.js
+++ src/svg/svg-transform.js
@@ -3,7 +3,6 @@ import Point from '@/models/point'
 class SvgTransform {
   init(svgElement) {
     this.svgElement = svgElement;
-    this.transformMatrix = this.svgElement.getScreenCTM().inverse();
   }
 
   getScreenPoint(event) {
@@ -23,7 +22,8 @@ class SvgTransform {
     let pt = this.svgElement.createSVGPoint();
     pt.x = event.clientX;
     pt.y = event.clientY;
-    pt = pt.matrixTransform(this.transformMatrix);
+    const transformMatrix = this.svgElement.getScreenCTM().inverse();
+    pt = pt.matrixTransform(transformMatrix);
     return new Point(
       pt.x,
       pt.y);
