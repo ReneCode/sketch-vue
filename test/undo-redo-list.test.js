2b09b1298f59a679fc2127010d3126aaf4b6899c
rene_mbp <harryloveslearning@googlemail.com>
2017-09-29 16:00:27 +0200

    ability to work localy (firebase-server)

---
+++ test/undo-redo-list.test.js
@@ -0,0 +1,12 @@
+
+let should = require('chai').should();
+
+import undoRedoList from '../src/store/modules/undo-redo-list'
+
+describe('UndoRedoList', () => {
+  it('should start with canUndo/Redo false', () => {
+    undoRedoList.canRedo.should.be.false;
+    undoRedoList.canUndo.should.be.false;
+  });
+
+});
